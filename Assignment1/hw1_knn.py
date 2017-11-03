cf2d20106211f9800f72b253acf359b3af830288
gaurav <saist1993@gmail.com>
2017-11-03 11:18:55 +0100

    Added basic file structure and solution of firt assignment

---
+++ Assignment1/hw1_knn.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on  
+
+@author: fame
+"""
+ 
+import numpy as np
+import traceback
+import collections
+
+ 
+
+def compute_distance(X,Y):
+    """
+
+    :param X: Input matrix 1
+    :param Y: Input matrix 2
+    :return: Euclidean distance between two matrix i.e a float value
+    """
+    return np.sqrt(((X - Y)**2).sum(-1))
+    # return np.linalg.norm(X-Y,axis=-1)
+
+def compute_euclidean_distances( X, Y ) :
+    """
+    Compute the Euclidean distance between two matricess X and Y
+    Input:
+    X: N-by-D numpy array
+    Y: M-by-D numpy array 
+    
+    Should return dist: M-by-N numpy array   
+    """
+    dist = np.zeros(shape=(len(Y), len(X)))
+    print dist.shape
+    for index_1 in xrange(0,len(Y)):
+        for index_2 in xrange(0,len(X)):
+            # print index_1,index_2
+            dist[index_1][index_2] = compute_distance(Y[index_1],X[index_2])
+    return dist
+
+def predict_labels( dists, labels, k=1):
+    """
+    Given a Euclidean distance matrix and associated training labels predict a label for each test point.
+    Input:
+    dists: M-by-N numpy array - M test examples and N training examples
+    labels: is a N dimensional numpy array
+    
+    Should return  pred_labels: M dimensional numpy array
+    """
+    print dists.shape
+    pred_label = []
+    for index in dists:
+        #Create a tuple and then sort sort the array and select top k candidates
+        s = np.argsort(index)
+        # print index
+        temp1= index[s]
+        # print "*************"
+        # print index
+        # print s
+        temp = labels[s][:k]
+        # print temp
+        # raw_input()
+        pred_label.append(collections.Counter(temp).most_common()[0][0])
+    pred_label = np.array(pred_label)
+    print pred_label.shape
+    return pred_label
---
+++ Assignment1/hw1_main_knn.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+Created on
+
+@author: fame
+"""
+
+from load_mnist import *
+import hw1_knn  as mlBasics
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
+# Load data - two class
+# X_train, y_train = load_mnist('training' , [0,1] )
+# X_test, y_test = load_mnist('testing'  ,  [0,1]  )
+
+
+def reshape_predict(X_train,y_train,X_test,y_test,confusion=False,k=1):
+    X_train = np.reshape(X_train, (X_train.shape[0], -1))
+    X_test = np.reshape(X_test, (X_test.shape[0], -1))
+
+    # Test on test data
+    # 1) Compute distances:
+    dists = mlBasics.compute_euclidean_distances(X_train, X_test)
+    print "I am here"
+
+    # 2) Run the code below and predict labels:
+    y_test_pred = mlBasics.predict_labels(dists, y_train,k=k)
+    if not confusion:
+        return np.mean(y_test_pred==y_test)*100
+    else:
+        return np.mean(y_test_pred==y_test)*100, confusion_matrix(y_test, y_test_pred)
+
+def five_fold_cross(X_train, y_train,k):
+    num_folds = 5
+    set_size = len(X_train)/num_folds
+    accuracy = []
+    for i in xrange(0,num_folds):
+        x_tests = X_train[i*set_size:]
+        y_tests = y_train[i*set_size:]
+        x_trains = np.concatenate((X_train[:i*set_size] ,X_train[(i+1)*set_size:]),axis=0)
+        # x_train = X_train[:i*set_size] + X_train[(i+1)*set_size:]y
+        y_trains = np.concatenate((y_train[:i*set_size] , y_train[(i+1)*set_size:]),axis=0)
+        # y_train = y_train[:i*set_size] + y_train[(i+1)*set_size:]
+        accuracy.append(reshape_predict(x_trains, y_trains, x_tests, y_tests,confusion=False,k=k))
+    return np.mean(np.array(accuracy))
+
+
+
+user_input = raw_input('enter question ')
+X_train, y_train = load_mnist('training')
+X_test, y_test = load_mnist('testing')
+
+if user_input == 1:
+    accuracy = reshape_predict(X_train,y_train,X_test,y_test)
+    print '{0:0.02f}'.format(accuracy), "of test examples classified correctly."
+
+if user_input == str(2):
+    #randomly shuffle X_train and Y_train and then take first 100 required sample.
+    s = np.arange(X_train.shape[0])
+    np.random.shuffle(s)
+    X_train = X_train[s]
+    y_train - y_train[s]
+
+    sample_set = {}
+    new_X_train = []
+    new_Y_train = []
+
+
+    for i in xrange(0,len(y_train)):
+        try:
+            count = sample_set[y_train[i]]
+            if count >= 100:
+                continue
+            else:
+                new_X_train.append(X_train[i])
+                new_Y_train.append(y_train[i])
+                sample_set[y_train[i]] = sample_set[y_train[i]] + 1
+        except:
+            # print len(sample_set)
+            sample_set[y_train[i]] = 1
+            new_X_train.append(X_train[i])
+            new_Y_train.append(y_train[i])
+
+    X_train = np.array(new_X_train)
+    y_train = np.array(new_Y_train)
+
+    accuracy, confusion_matrix = reshape_predict(X_train,y_train, X_test, y_test,confusion=True)
+    print '{0:0.02f}'.format(accuracy), "of test examples classified correctly."
+    print confusion_matrix
+
+if user_input == str(3):
+    #randomly shuffle X_train and Y_train and then take first 100 required sample.
+    print "executing the k-fold task"
+    s = np.arange(X_train.shape[0])
+    np.random.shuffle(s)
+    X_train = X_train[s]
+    y_train - y_train[s]
+
+    sample_set = {}
+    new_X_train = []
+    new_Y_train = []
+
+
+    for i in xrange(0,len(y_train)):
+        try:
+            count = sample_set[y_train[i]]
+            if count >= 100:
+                continue
+            else:
+                new_X_train.append(X_train[i])
+                new_Y_train.append(y_train[i])
+                sample_set[y_train[i]] = sample_set[y_train[i]] + 1
+        except:
+            # print len(sample_set)
+            sample_set[y_train[i]] = 1
+            new_X_train.append(X_train[i])
+            new_Y_train.append(y_train[i])
+
+    X_train = np.array(new_X_train)
+    y_train = np.array(new_Y_train)
+    accuracy = []
+    for k in xrange(0,15):
+        accuracy.append(five_fold_cross(X_train,y_train,k=k+1))
+        print "done with ", k+1
+    print
+    # [58.358333333333334, 35.806666666666665, 29.791666666666664, 27.41833333333334, 25.994999999999997,
+    #  23.239999999999998, 22.66, 21.876666666666665, 20.890000000000001, 19.140000000000001, 18.119999999999997,
+    #  17.883333333333333, 17.350000000000001, 16.971666666666668, 16.801666666666666]
+
